0fe8df11900009b0ec667a727803744e7392f346
Zeynep Sude Cengiz <cengizzsude@gmail.com>
2025-04-28 11:52:32 +0300

    feat: Add global exception handling

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from backend.internal.exception.handler import (
+    http_exception_handler,
+    validation_exception_handler,
+    generic_exception_handler,
+    base_exception_handler
+)
+from backend.internal.exception.base_exception import BaseException
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException as StarletteHTTPException
 from backend.routes.auth_controller import router as auth_router
 
 app = FastAPI()
 
+app.add_exception_handler(StarletteHTTPException, http_exception_handler)
+app.add_exception_handler(RequestValidationError, validation_exception_handler)
+app.add_exception_handler(Exception, generic_exception_handler)
+app.add_exception_handler(BaseException, base_exception_handler)
 app.include_router(auth_router, prefix="/api/auth", tags=["Authentication"])
 
 @app.get("/")
